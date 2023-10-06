1645b3af7401d20be64f16edec3b4d44e10e22f7
Liudas Panavas <panavas.l@northeastern.edu>
2023-10-06 11:07:11 -0400

    additional parameters

--- src/App.js
+++ src/App.js
@@ -7,24 +7,46 @@ import "rc-slider/assets/index.css";
 function App() {
   const MIN_EPSILON = 0.1;
   const MAX_EPSILON = 10;
-  const sensitivity = 1;
 
   const [sliderValues, setSliderValues] = useState([
     MIN_EPSILON,
     (MIN_EPSILON + MAX_EPSILON) / 2,
     MAX_EPSILON,
   ]);
-  const k = 1; // example value for sensitivity
-  const delta = 0.05; // example value for delta
+  const [queryType, setQueryType] = useState("Count");
+  const [numBins, setNumBins] = useState(1);
+  const [datasetBounds, setDatasetBounds] = useState({
+    low: 0,
+    high: 100,
+  });
+  const [datasetSize, setDatasetSize] = useState(100);
+
+  const delta = 0.05;
+  const delta_gaussian = 0.001;
+
+  const k = queryType === "Histogram" ? numBins : 1;
+  const sensitivity = (() => {
+    if (queryType === "Sum") {
+      return datasetBounds.high - datasetBounds.low;
+    }
+    if (queryType === "Mean") {
+      // Adjust the sensitivity for mean based on dataset size
+      return (datasetBounds.high - datasetBounds.low) / datasetSize;
+    }
+    return 1;
+  })();
+
   const errorForSelectedEpsilon =
     Math.log(k / delta) * (sensitivity / sliderValues[1]);
+  // const sigma =
+  //   (Math.sqrt(k) *
+  //     (sensitivity * Math.sqrt(2 * Math.log(1.25 / delta_gaussian)))) /
+  //   sliderValues[1];
+  // const gaussianErrorForSelectedEpsilon = 1.96 * sigma;
+
   return (
     <div className="app-container">
-      <div className="app-section">
-        With probability {((1 - delta) * 100).toFixed(2)}%, the private
-        statistical release will not be off by more than{" "}
-        {errorForSelectedEpsilon.toFixed(2)}.
-      </div>
+      {/* Chart */}
       <div className="app-section">
         <LineChart
           width={600}
@@ -32,8 +54,105 @@ function App() {
           minEpsilon={sliderValues[0]}
           maxEpsilon={sliderValues[2]}
           selectedEpsilon={sliderValues[1]}
+          k={k}
+          sensitivity={sensitivity}
         />
       </div>
+
+      {/* Query Selection */}
+      <div className="app-section">
+        <label>
+          Select query type:
+          <select
+            value={queryType}
+            onChange={(e) => setQueryType(e.target.value)}
+          >
+            <option value="Count">Count</option>
+            <option value="Sum">Sum</option>
+            <option value="Mean">Mean</option>
+            <option value="Histogram">Histogram</option>
+          </select>
+        </label>
+      </div>
+
+      {/* Conditional Inputs */}
+      {queryType === "Histogram" && (
+        <div className="app-section">
+          <label>
+            Number of bins:
+            <input
+              type="number"
+              value={numBins}
+              onChange={(e) => setNumBins(parseInt(e.target.value))}
+            />
+          </label>
+        </div>
+      )}
+
+      {(queryType === "Sum" || queryType === "Mean") && (
+        <div className="app-section">
+          <label>
+            Estimated Low Value of Dataset:
+            <input
+              type="number"
+              value={datasetBounds.low}
+              onChange={(e) =>
+                setDatasetBounds((prev) => ({
+                  ...prev,
+                  low: parseInt(e.target.value),
+                }))
+              }
+            />
+          </label>
+          <br />
+          <label>
+            Estimated High Value of Dataset:
+            <input
+              type="number"
+              value={datasetBounds.high}
+              onChange={(e) =>
+                setDatasetBounds((prev) => ({
+                  ...prev,
+                  high: parseInt(e.target.value),
+                }))
+              }
+            />
+          </label>
+        </div>
+      )}
+      {queryType === "Mean" && (
+        <div className="app-section">
+          <label>
+            Dataset Size:
+            <input
+              type="number"
+              value={datasetSize}
+              onChange={(e) =>
+                setDatasetSize(Math.max(1, parseInt(e.target.value)))
+              }
+            />
+          </label>
+        </div>
+      )}
+
+      {/* Error Display */}
+      <div className="app-section">
+        With a confidence level of {((1 - delta) * 100).toFixed(2)}%:
+        <ul>
+          <li>
+            The error of the Laplace mechanism's private statistical estimate is
+            expected to be less than or equal to{" "}
+            {errorForSelectedEpsilon.toFixed(2)}.
+          </li>
+          {/* <li>
+            The error of the Gaussian mechanism's private statistical estimate
+            is expected to be less than or equal to{" "}
+            {gaussianErrorForSelectedEpsilon.toFixed(2)}.
+          </li> */}
+        </ul>
+      </div>
+
+      {/* Slider */}
       <div className="app-section">
         <label>
           Epsilon Range: {sliderValues[0].toFixed(2)} -{" "}
@@ -48,7 +167,7 @@ function App() {
           step={0.01}
           value={sliderValues}
           onChange={(values) => setSliderValues(values)}
-          allowCross={false} // This ensures handles don't cross each other
+          allowCross={false}
         />
       </div>
     </div>
--- src/Components/LineChart.js
+++ src/Components/LineChart.js
@@ -1,16 +1,25 @@
 import React, { useRef, useEffect } from "react";
 import * as d3 from "d3";
+import { quantile } from "d3-array";
 
-function LineChart({ width, height, minEpsilon, maxEpsilon, selectedEpsilon }) {
+function LineChart({
+  width,
+  height,
+  minEpsilon,
+  maxEpsilon,
+  selectedEpsilon,
+  k, // <- added k
+  sensitivity, // <- added sensitivity
+}) {
   const svgRef = useRef(null);
 
   useEffect(() => {
     const svg = d3.select(svgRef.current);
-    const k = 1; // example value
-    const delta = 0.05; // example value
-    const sensitivity = 1; // example value
-    // Data generation using the Laplace error formula (error vs. epsilon)
-    const data = Array.from({ length: 100 }, (_, index) => {
+    const delta = 0.05;
+    const delta_gaussian = 0.00001; // Adjust as per your requirements
+
+    // Laplace error data
+    const dataLaplace = Array.from({ length: 100 }, (_, index) => {
       const computedEpsilon =
         minEpsilon + (maxEpsilon - minEpsilon) * (index / 99);
       const error = Math.log(k / delta) * (sensitivity / computedEpsilon);
@@ -20,15 +29,38 @@ function LineChart({ width, height, minEpsilon, maxEpsilon, selectedEpsilon }) {
       };
     });
 
+    // Gaussian error data
+    // const dataGaussian = Array.from({ length: 100 }, (_, index) => {
+    //   const computedEpsilon =
+    //     minEpsilon + (maxEpsilon - minEpsilon) * (index / 99);
+    //   const sigma =
+    //     (sensitivity * Math.sqrt(2 * Math.log(1.25 / delta_gaussian) * k)) / // <- Adjusted for sqrt(k)
+    //     computedEpsilon;
+    //   const error = sigma * 1.96; // 95% confidence interval for Gaussian noise
+    //   console.log(
+    //     `Epsilon: ${computedEpsilon}, Sigma: ${sigma}, Sensitivity: ${sensitivity}, Delta: ${delta_gaussian}`
+    //   ); // Debugging line
+
+    //   return {
+    //     epsilon: computedEpsilon,
+    //     error: error,
+    //   };
+    // });
+
     const xScale = d3
       .scaleLinear()
       .domain([minEpsilon, maxEpsilon])
-      .range([50, width - 10]); // Padding for axis
-
+      .range([60, width - 10]);
     const yScale = d3
       .scaleLinear()
-      .domain([0, d3.max(data, (d) => d.error)])
-      .range([height - 30, 10]); // Padding for axis
+      .domain([
+        0,
+        Math.max(
+          d3.max(dataLaplace, (d) => d.error)
+          //   d3.max(dataGaussian, (d) => d.error)
+        ),
+      ])
+      .range([height - 50, 10]);
 
     const line = d3
       .line()
@@ -38,38 +70,88 @@ function LineChart({ width, height, minEpsilon, maxEpsilon, selectedEpsilon }) {
     svg.selectAll("*").remove();
     svg
       .append("g")
-      .attr("transform", `translate(0,${height - 30})`) // Positioning at the bottom with padding
+      .attr("transform", `translate(0,${height - 50})`)
       .call(d3.axisBottom(xScale).ticks(10));
-
-    // Draw Y axis with labels
     svg
       .append("g")
-      .attr("transform", "translate(50,0)") // Positioning with padding
+      .attr("transform", "translate(60,0)")
       .call(d3.axisLeft(yScale));
-    svg
-      .append("g")
-      .attr("transform", `translate(0,${height})`)
-      .call(d3.axisBottom(xScale).ticks(10));
 
-    // svg.append("g").call(d3.axisLeft(yScale));
-
+    // Laplace error line
     svg
       .append("path")
-      .datum(data)
+      .datum(dataLaplace)
       .attr("fill", "none")
       .attr("stroke", "blue")
       .attr("d", line);
 
+    // Gaussian error line
+    // svg
+    //   .append("path")
+    //   .datum(dataGaussian)
+    //   .attr("fill", "none")
+    //   .attr("stroke", "green")
+    //   .attr("d", line);
+
+    // Epsilon line
     const selectedXPosition = xScale(selectedEpsilon);
     svg
       .append("line")
       .attr("x1", selectedXPosition)
-      .attr("y1", 10) // top padding
+      .attr("y1", 10)
       .attr("x2", selectedXPosition)
-      .attr("y2", height - 30) // bottom position minus padding
+      .attr("y2", height - 30)
       .attr("stroke", "red")
-      .attr("stroke-dasharray", "4"); // Optional: dashed line style
-  }, [minEpsilon, maxEpsilon, width, height, selectedEpsilon]);
+      .attr("stroke-dasharray", "4");
+
+    // Legend
+    const legend = svg
+      .append("g")
+      .attr("transform", `translate(${width - 150},20)`);
+    legend
+      .append("circle")
+      .attr("cx", 10)
+      .attr("cy", 0)
+      .attr("r", 6)
+      .style("fill", "blue");
+    // legend
+    //   .append("circle")
+    //   .attr("cx", 10)
+    //   .attr("cy", 30)
+    //   .attr("r", 6)
+    //   .style("fill", "green");
+    legend
+      .append("text")
+      .attr("x", 30)
+      .attr("y", 0)
+      .text("Laplace Error")
+      .style("font-size", "15px")
+      .attr("alignment-baseline", "middle");
+    // legend
+    //   .append("text")
+    //   .attr("x", 30)
+    //   .attr("y", 30)
+    //   .text("Gaussian Error")
+    //   .style("font-size", "15px")
+    //   .attr("alignment-baseline", "middle");
+
+    // Axis labels
+    svg
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 5)
+      .attr("x", -height / 2)
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text("Predicted Error (95% confidence)");
+    svg
+      .append("text")
+      .attr("x", width / 2)
+      .attr("y", height - 5) // This will move the label above the x-axis
+      .attr("dy", "-0.5em")
+      .style("text-anchor", "middle")
+      .text("Epsilon (ε)");
+  }, [minEpsilon, maxEpsilon, width, height, selectedEpsilon, k, sensitivity]);
 
   return <svg ref={svgRef} width={width} height={height}></svg>;
 }
